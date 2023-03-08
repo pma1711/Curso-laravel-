be55f96c4f78a2caf630d11358853d5b3550761e
pma1711 <pma1711@gmail.com>
2023-03-08 11:51:19 -0600

    form error

---
+++ resources/views/dashboard/fragment/_errors-form.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    @if ($errors->any())
+    @foreach ($errors->all() as $e )
+        <div class="error">
+            {{$e}}
+        </div>
+    @endforeach
+    @endif
+</body>
+</html>
--- resources/views/dashboard/post/create.blade.php
+++ resources/views/dashboard/post/create.blade.php
@@ -9,14 +9,9 @@
 <body>
     <h1>Crear Post</h1>
 
-    @if ($errors->any())
-    @foreach ($errors->all() as $e )
-        <div class="error">
-            {{$e}}
-        </div>
-    @endforeach
-        
-    @endif
+    @include('dashboard.fragment._errors-form')
+
+    
 <form action="{{route ('post.store') }}" method="post">
     @csrf
 
