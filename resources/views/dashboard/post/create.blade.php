dc22d823d9ff6c7c7376ca3145779141d4a360d3
pma1711 <pma1711@gmail.com>
2023-03-08 11:57:15 -0600

    vista madre

--- resources/views/dashboard/post/create.blade.php
+++ resources/views/dashboard/post/create.blade.php
@@ -1,45 +1,37 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Crear post</title>
-</head>
-<body>
-    <h1>Crear Post</h1>
+@extends('dashboard.layout')
+@section('content')
+<h1>Crear Post</h1>
 
-    @include('dashboard.fragment._errors-form')
+@include('dashboard.fragment._errors-form')
 
-    
+
 <form action="{{route ('post.store') }}" method="post">
-    @csrf
+@csrf
 
-    <label for="">Titulo</label>
-    <input type="text" name="title">
+<label for="">Titulo</label>
+<input type="text" name="title">
 
-    <label for="">Slug</label>
-    <input type="text" name="slug">
+<label for="">Slug</label>
+<input type="text" name="slug">
 
-    <label for="">Categoria</label>
-    <select name="category_id">
-        <option value=""></option>
-           @foreach ($categories as $title=> $id)
-           <option value="{{$id}}">{{$title}}</option>
-               
-           @endforeach
-    </select>
+<label for="">Categoria</label>
+<select name="category_id">
+    <option value=""></option>
+       @foreach ($categories as $title=> $id)
+       <option value="{{$id}}">{{$title}}</option>
+           
+       @endforeach
+</select>
 <label for="">Posteado</label>
-    <select name="posted">
-        <option value="no">No</option>
-        <option value="yes">si</option>
-    </select>
-    <label for="">Contenido</label>
-    <textarea name="content"></textarea>
+<select name="posted">
+    <option value="no">No</option>
+    <option value="yes">si</option>
+</select>
+<label for="">Contenido</label>
+<textarea name="content"></textarea>
 
-    <label for="">Descripcion</label>
-    <textarea name="description"></textarea>
+<label for="">Descripcion</label>
+<textarea name="description"></textarea>
 
-    <button type="submit">Enviar</button>
-</body>
-</html>
+<button type="submit">Enviar</button>
+@endsection
